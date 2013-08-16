f4833070a090ee4e37d267fd7a220717c2aa594c
grzegorz123 <wa258827@inetia.pl>
2013-08-16 10:38:23 +0200

    Dodana nowa nazwa uzytkownika do szkicu HelloFork.ino

--- HelloFork.ino
+++ HelloFork.ino
@@ -1,16 +1,14 @@
 /*
-* Hello Fork Github Example Arduino Sketch
-* Just add your GitHUb account ID and I'll add you pull request to the project.
+* Przykład szkicu Arduino HelloFork
+* Dodaj nazwę swojego konta w GitHub i żądanie zmian do projektu
 */
-
 void setup() {
   Serial.begin(9600);
 }
-
 void loop() {
-  Serial.println("Add your GitHub name to the code to test creating a pull request");
-  Serial.println("Hello Github from:");
-  Serial.println("@Ricklon");
-  Serial.println("@ProArd");
-  Serial.println("@Hotemup");
+  Serial.println("Dodaj swoją nazwę GitHub aby sprawdzić żądanie zmian");
+  Serial.println("Witaj Github od:");
+  Serial.println("@TwojaNazwa1");
+  Serial.println("@TwojaNazwa2");
+  Serial.println("@TwojaNazwaGitHub");
 }
